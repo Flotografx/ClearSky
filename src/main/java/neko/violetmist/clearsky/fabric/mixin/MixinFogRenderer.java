0340f4b75f75ac3cbac2445bb446b72cb02cbca3
Flotografx <git@flotografx.de>
2025-01-13 13:43:36 +0100

    🎉 Fabric 1.21.3 port!

--- src/main/java/neko/violetmist/clearsky/fabric/mixin/MixinFogRenderer.java
+++ src/main/java/neko/violetmist/clearsky/fabric/mixin/MixinFogRenderer.java
@@ -15,8 +15,8 @@ public abstract class MixinFogRenderer {
             at = @At(
                     value = "INVOKE_ASSIGN",
                     target = "Lnet/minecraft/util/CubicSampler;gaussianSampleVec3(Lnet/minecraft/world/phys/Vec3;Lnet/minecraft/util/CubicSampler$Vec3Fetcher;)Lnet/minecraft/world/phys/Vec3;"),
-            method = "setupColor",
-            ordinal = 2,
+            method = "computeFogColor",
+            ordinal = 1,
             require = 1,
             allow = 1)
     private static Vec3 onSampleColor(Vec3 val) {
@@ -24,23 +24,23 @@ public abstract class MixinFogRenderer {
         final ClientLevel world = mc.level;
         if (world == null) return val;
         if (world.dimensionType().hasSkyLight()) {
-            return world.getSkyColor(mc.gameRenderer.getMainCamera().getPosition(), mc.getFrameTimeNs());
+            return Vec3.fromRGB24(world.getSkyColor(mc.gameRenderer.getMainCamera().getPosition(), mc.getFrameTimeNs()));
         } else {
             return val;
         }
     }
 
-    @ModifyVariable(at = @At(value = "INVOKE_ASSIGN", target = "Lorg/joml/Vector3f;dot(Lorg/joml/Vector3fc;)F", remap = false), method = "setupColor", ordinal = 7, require = 1, allow = 1)
+    @ModifyVariable(at = @At(value = "INVOKE_ASSIGN", target = "Lorg/joml/Vector3f;dot(Lorg/joml/Vector3fc;)F", remap = false), method = "computeFogColor", ordinal = 5, require = 1, allow = 1)
     private static float afterPlaneDot(float f) {
         return 0;
     }
 
-    @Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/multiplayer/ClientLevel;getRainLevel(F)F"), method = "setupColor", require = 1, allow = 1)
+    @Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/multiplayer/ClientLevel;getRainLevel(F)F"), method = "computeFogColor", require = 1, allow = 1)
     private static float onGetRainLevel(ClientLevel world, float tickDelta) {
         return 0;
     }
 
-    @Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/multiplayer/ClientLevel;getThunderLevel(F)F"), method = "setupColor", require = 1, allow = 1)
+    @Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/multiplayer/ClientLevel;getThunderLevel(F)F"), method = "computeFogColor", require = 1, allow = 1)
     private static float onGetThunderLevel(ClientLevel world, float tickDelta) {
         return 0;
     }
